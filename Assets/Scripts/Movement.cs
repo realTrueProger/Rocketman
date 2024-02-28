39e71b1a8512e7f19ea301bf475949fc2ed952a6
Vladimir Solovev <vsolovev@inno.tech>
2024-02-28 21:06:39 +0300

    test commit

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -18,6 +18,7 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("Test commit");
         ProcessUp();
         ProcessRotation();
     }
